7d67a9866b134e41ff7611d7b32f7d99dd72d282
João Vitor Pinheiro da Costa <joaovpro17@gmail.com>
2022-03-15 00:44:52 -0400

    update dio.sql exercise

--- cognizant_data_engineer/códigos/sql/dio.sql
+++ cognizant_data_engineer/códigos/sql/dio.sql
@@ -7,3 +7,21 @@ CREATE TABLE pessoas (
 INSERT INTO pessoas (nome, nascimento) VALUES ('Mbappe', '2000-09-15')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Neymar', '1993-05-22')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Messi', '1985-01-10')
+
+UPDATE pessoas SET nome = 'Ney JR' WHERE id = 2;
+
+DELETE FROM pessoas WHERE id = 3;
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('João Vitor', '1998-09-15');
+
+SELECT * FROM pessoas ORDER BY nome;
+
+SELECT * FROM pessoas ORDER BY nascimento DESC;
+
+ALTER TABLE pessoas ADD posicao VARCHAR(15) NOT NULL BEFORE nascimento;
+
+UPDATE pessoas SET posicao = 'ATACANTE' WHERE id = 1;
+UPDATE pessoas SET posicao = 'ATACANTE' WHERE id = 2;
+
+SELECT COUNT(id), posicao FROM pessoa GROUP BY posicao;
+
